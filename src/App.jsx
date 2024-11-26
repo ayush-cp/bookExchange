50419451485bbb73dc959c7ad656ec8f42da31c8
wbidit <brb020@nist.edu>
2024-11-26 16:19:39 +0530

    profilepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ProfilePage from './components/ProfilePage';
+import { Camera } from 'lucide-react';
 import Home from "./components/Home";
 import BookSearch from "./components/bookSearch";
 
@@ -8,11 +10,13 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/bookSearch" element={<BookSearch />} />
+          
+        <Route path="/bookSearch" element={<BookSearch />} />
+        <Route path="/" element={<ProfilePage />} />
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
 
-export default App;
+export default App;
